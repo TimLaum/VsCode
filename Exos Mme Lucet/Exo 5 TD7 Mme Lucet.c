ffb42709e2e13c77158715df41e324e0f9e4d724
iXwin <timothee.laumonier@gmail.com>
2022-11-08 11:47:28 +0100

    Exercices TD7 (5 pas fini)

--- Exos Mme Lucet/Exo 5 TD7 Mme Lucet.c
+++ Exos Mme Lucet/Exo 5 TD7 Mme Lucet.c
@@ -2,23 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-int aleaCharactere() {
+char aleaCharactere(){
     int i;
     char alea;
-    srand((unsigned int) time(NULL));
-    for(i = 0; i < 3; i++) {
-        alea = rand() % 26 + 'a' ;
-    }
+    alea = (rand() % 26) + 97 ;
     return alea;
 }
 
-void Tableau(){
+void initialisation(){
+    srand(time(NULL));
     int i=0,j=0;
-    char tab[10][20],aleatoire;
+    char tab[10][20];
     for (int i = 0; i < 10 ; ++i) {
         for (int j = 0; j < 20 ; ++j) {
-            aleatoire = aleaCharactere();
-            tab[i][j] = aleatoire;
+            tab[i][j] =aleaCharactere();
         }
         printf("%c",tab[i][j]);
     }
@@ -26,5 +23,5 @@ void Tableau(){
 }
 
 int main(){
-    Tableau();
+    initialisation();
 }
